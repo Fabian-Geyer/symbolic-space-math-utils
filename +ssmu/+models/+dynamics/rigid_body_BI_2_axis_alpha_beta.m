661af4ba1bfec44b94f0b79433d44c4108ee0d9d
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2025-09-08 15:49:19 +0200

    add reduced kinematics using alpha and beta for parametrization

---
+++ +ssmu/+models/+dynamics/rigid_body_BI_2_axis_alpha_beta.m
@@ -0,0 +1,31 @@
+function [alpha_dot, beta_dot, omega_BI_B_dot] = ...
+    rigid_body_BI_2_axis_alpha_beta(alpha, beta,omega_BI_B, J, tau_B)
+% returns the basic dynamics of a rigid body with respect to the inertial
+% frame for 2-axis acquisition.
+% Inputs:
+%   - s_BI: 3x1 attitude direction vector expressed in body frame w.r.t.
+%   inertial frame I. s_BI = (s1,s2,s3)^T
+%   - omega_BI_B: 3x1 angular rate vector of B frame w.r.t. I frame
+%   expressed in B
+%   - J: 3x3 (potentially symbolic) inertia matrix
+%   - tau_B: 3x1 (symbolic) sum of torques acting on the satellite
+
+arguments
+    alpha (1,1)
+    beta (1,1)
+    omega_BI_B (3,1)
+    J (3,3)
+    tau_B (3,1)
+end
+
+om = omega_BI_B;
+
+% Reduced Kinematics when using alpha and beta
+% Transformation: T_BA = T2(alpha)*T3(-beta)
+alpha_dot =  -(om1*cos(alpha)*sin(beta) - om2*cos(beta) + om3*sin(alpha)*sin(beta))/cos(beta);
+beta_dot  = om1*sin(alpha) - om3*cos(alpha);
+
+omega_BI_B_dot = -inv(J)*( ssmu.cpm(om)*J*om - tau_B ); % dynamics
+
+end
+
